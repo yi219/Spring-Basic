8bb06601eea4a0bc236bc9eaf4cb43f512a480a7
yi219 <tbc07061@naver.com>
2023-08-23 02:04:16 +0900

    빈 생명주기 콜백

--- src/test/java/hello/core/xml/XmlAppContext.java
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -1,6 +1,9 @@
 package hello.core.xml;
 
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -11,7 +14,21 @@ public class XmlAppContext {
     @Test
     void xmlAppContext(){
         ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
-        MemberService memberService = ac.getBean("memberService", MemberService.class);
-        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+//        MemberService memberService = ac.getBean("memberService", MemberService.class);
+//        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberRepository1 = " + memberRepository1);
+        System.out.println("memberRepository2 = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberRepository1).isSameAs(memberRepository);
+        Assertions.assertThat(memberRepository2).isSameAs(memberRepository);
     }
 }
